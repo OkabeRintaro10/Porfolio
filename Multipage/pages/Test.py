12d3b1faab062f27d3df873855d320b0e95a5dd3
OkabeRintaro10 <kausikremella@gmail.com>
2023-09-13 11:42:48 +0530

    changes

--- Multipage/pages/Test.py
+++ Multipage/pages/Test.py
@@ -16,28 +16,6 @@ def get_data(filename):
     data = pd.read_csv(filename, on_bad_lines="skip", verbose=False)
     return data
 st.title("Bus Stops")
-st.subheader("Total bus stops")
-df = get_data("Dataset/020230701APSRTC_BOOKED_TICKETS.csv")
-st.write(df.head())
-buffer = io.StringIO()
-df.info(buf=buffer)
-s = buffer.getvalue()
-
-st.text(s)
-
-st.subheader("Relevant info")
-
-df = df[['Date of Journey', 'Traveled From', 'Traveled To', 'Route Description',
-        'DepotName', 'ServiceStartDate', 'ServiceStartTime', 'BookedDate', 'RegionName',
-        'DepartureTime', 'OriginCityName', 'DestinationCity',
-        'Created Date Time', 'Arrival Time']]
-
-st.write(df.head())
-buffer = io.StringIO()
-df.info(buf=buffer)
-s = buffer.getvalue()
-
-st.text(s)
 
 st.subheader("Bus stops in AP")
 BusStops = get_data("Dataset/Busstop.csv")
