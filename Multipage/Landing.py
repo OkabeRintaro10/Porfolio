c5cb518f16acf0788680373cc7c421e2d20ddf74
OkabeRintaro10 <kausikremella@gmail.com>
2023-09-13 11:31:16 +0530

    Removal

--- Multipage/Landing.py
+++ Multipage/Landing.py
@@ -6,11 +6,3 @@ st.set_page_config(
 
 st.title("Main Page")
 st.sidebar.success("Select Page from above")
-
-st.title('Counter Example')
-increment = st.button('Increment')
-count = 0
-if increment:
-    count += 1
-
-st.write('count = ', count)
