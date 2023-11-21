c47c2c43f57fdea069cb28fb8c1657dc55be9087
OkabeRintaro10 <kausikremella@gmail.com>
2023-11-21 22:25:36 +0530

    Fix image display issue and improve face recognition

--- Multipage/pages/FaceRecognition.py
+++ Multipage/pages/FaceRecognition.py
@@ -2,9 +2,11 @@ from deepface import DeepFace
 import cv2
 import time
 import streamlit as st
+from PIL import Image
 
 windowsHolder = st.empty()
 value = st.empty()
+
 def main():
     cap = cv2.VideoCapture(0)
     while True:
@@ -15,7 +17,7 @@ def main():
             break
 
         #cv2.imshow("Window", img)
-        windowsHolder.image(img,channels="BGR")
+        windowsHolder.image(img, channels="BGR")
         extract_faces(img)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
@@ -24,9 +26,9 @@ def main():
 
     return
 
+
 def extract_faces(raw_img):
-    print("Starting recognition")
-    test = DeepFace.extract_faces(raw_img, detector_backend="mtcnn",align=True)
+    test = DeepFace.extract_faces(raw_img, detector_backend="mtcnn", align=True)
 
     if test:
         faces = []
@@ -47,17 +49,20 @@ def extract_faces(raw_img):
             detected_face = raw_img[int(y) : int(y + h), int(x) : int(x + w)]
             detected_faces.append(detected_face)
             recognition(detected_face)
+            print("Face detected")
+            time.sleep(0.6)
             return
 
+
 def recognition(img):
+    print("Starting recognition")
     dfs = DeepFace.find(
         img_path=img,
         db_path="/Users/futuregadgetlab/Desktop/DB",
         detector_backend="mtcnn",
-        model_name="Facenet512",
+        model_name="VGG-Face",
         distance_metric="euclidean_l2",
         enforce_detection=False,
-        silent=True
     )
 
     found_non_empty_df = False
@@ -65,7 +70,7 @@ def recognition(img):
     for df in dfs:
         if len(df) != 0:
             for _, row in df.iterrows():
-                if row["Facenet512_euclidean_l2"] < 0.6:
+                if row["VGG-Face_euclidean_l2"] < 0.6:
                     print("Matched!")
                     with value.container():
                         st.write("Matched! Welcome to expo 2023")
@@ -75,10 +80,13 @@ def recognition(img):
 
             if found_non_empty_df:
                 break  # Exit the outer loop if a match is found in any DataFrame
-        time.sleep(5)
 
     if not found_non_empty_df:
+        print("SIke")
         with value.container():
             st.warning("You don't exist, my friend!")
+    time.sleep(1)
+
+
 if __name__ == "__main__":
     main()
